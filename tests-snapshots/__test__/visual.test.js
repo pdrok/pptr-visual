be0d25e361d68f4bd33b33f4b02e32893b3d9272
Pedro Lopez <plopez@poweranalytics.com>
2021-04-05 14:38:40 -0400

    Implement Visual Regression Test

---
+++ tests-snapshots/__test__/visual.test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const { toMatchImageSnapshot } = require('jest-image-snapshot')
+
+expect.extend({ toMatchImageSnapshot })
+
+describe('Visual Regression Testing', () => {
+	let browser
+	let page
+
+	beforeAll(async function () {
+		browser = await puppeteer.launch({ headless: true })
+		page = await browser.newPage()
+	})
+
+	afterAll(async function () {
+		await browser.close()
+	})
+
+	test('Full Page Snapshot', async function () {
+		await page.goto('https://www.example.com')
+		await page.waitForSelector('h1')
+		const image = await page.screenshot()
+		expect(image).toMatchImageSnapshot({
+			failureTresholdType: 'pixel',
+			failureTreshold: 500,
+		})
+	})
+})
