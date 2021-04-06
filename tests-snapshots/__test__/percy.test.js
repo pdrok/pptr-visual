1348ab67db1ad0ba81fbc567af09ce73ad3c7753
Pedro Lopez <plopez@poweranalytics.com>
2021-04-06 10:36:34 -0400

    Percy - Integration with Puppeteer

---
+++ tests-snapshots/__test__/percy.test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer')
+const { percySnapshot } = require('@percy/puppeteer')
+
+describe('Percy Visual Test', () => {
+	let browser
+	let page
+
+	beforeAll(async function () {
+		browser = await puppeteer.launch({ headless: true })
+		page = await browser.newPage()
+	})
+
+	afterAll(async function () {
+		await browser.close()
+	})
+
+	test('Full Page Percy Snapshot', async () => {
+		await page.goto('https://www.example.com')
+		await page.waitForTimeout(1000)
+		await percySnapshot(page, 'Example Page')
+	})
+})
